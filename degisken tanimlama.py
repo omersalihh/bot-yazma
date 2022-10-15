2c7da6fdbc7fa2c33916cbf015bd5682312d28b0
sgroen <114801151+omersalihh@users.noreply.github.com>
2022-10-15 09:35:03 +0300

    Add files via upload

---
+++ degisken tanimlama.py
@@ -0,0 +1,18 @@
+#degişkenler olmadan 2 öğrencinin sınav notlarını hesaplayalım
+
+#print( (80*0.4) + (75*0.6) )
+
+#print( (45*0.4) + (60*0.6) )
+
+#değişkenlerle 2 öğrencinin sınav notlarını hesaplayalım
+
+x = 0.5
+y = 0.5
+
+#print( (80*x) + (75*y) )
+#print( (45*x) + (60*y) )
+
+#değişken tanımlama kuralları
+
+#rakam ile başlayamaz
+#büyük ve küçük harfe duyarlıdır
