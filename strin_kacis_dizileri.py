9f8b1252393e1843be6803480f75b6c66edf9a54
omersalihh <salihomer4589@gmail.com>
2022-10-22 11:24:55 +0300

    string kaçış

---
+++ strin_kacis_dizileri.py
@@ -0,0 +1,19 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""
+
+print("ahmet, \"bugün sinenmaya gidiyorum \"dedi")
+
+print('yarın adana\'ya gidiyorum')
+
+print("C:\\Users\\Tasarım\\Desktop\\eski dosyalar
+
+#özel karakterler
+
+#  \n = alt satır
+
+#  \t = tab'a basar
+ 
