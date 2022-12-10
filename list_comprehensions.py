3797c32db4ef89c33083f484ff3d7583f3bbcd2f
sgroen <114801151+omersalihh@users.noreply.github.com>
2022-12-10 11:15:10 +0300

    Create list_comprehensions.py
    a

---
+++ list_comprehensions.py
@@ -0,0 +1,21 @@
+numaralar = []
+for i in range(100, 200):
+    numaralar.append(i)
+print(numaralar)
+
+
+
+numaralar2 = [sayi for sayi in range(100,200)]
+print(numaralar2)
+
+ciftsayilar = [sayi for sayi in range(100,200) if sayi %2 ==0]
+print(ciftsayilar)
+
+numaralar3 = [f"{sayi} TEK" if sayi %2 ==1 else f"{sayi} ÇİFT" for sayi in range(100,200)]
+print(numaralar3)
+
+liste = []
+for x in range(3):
+    for f in range(3);
+    liste.append((x, f))
+print(liste)
