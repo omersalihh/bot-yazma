a10ee7ff5df7514d78a8568c52bc3e48d5aae75f
sgroen <114801151+omersalihh@users.noreply.github.com>
2022-12-10 12:17:31 +0300

    salihh

--- fonksyon_parametreleri.py
+++ fonksyon_parametreleri.py
@@ -1,9 +1,25 @@
-ad = input("adınız: ")
-soy = input("soyadınız: ")
-notlar = input("sınav notları (virgülle ayrılmış)")
+# Öğrencinin adını, soyadını ve notlarını alıp ortalamasını
+# dosyaya yazdıran bir program yazalım
 
-notlar_list = [int(x)for x in notlar.split(",")]
-ortalama = sum(notlar_list)/len(notlar_list)
+# Öğrenci kaydetme fonksiyonu
+def ogrenci_kaydet(ad, soyad, notlar):
+    # notları integer yap ve ortalamayı hesapla
+    notlar_list = [int(x) for x in notlar.split(",")]
+    ortalama = sum(notlar_list) / len(notlar_list)
 
-dosya = open("ogreniler.txt",   mode="a")
-dosya.write(f"ad:{ad} , soyad:{soy} , notlar:{notlar} , ortalama:{notlar_list}")
+    # dosyaya yaz
+    dosya = open("öğrenciler.txt", mode="a", encoding="utf-8")
+    dosya.write(f"Ad: {ad}, Soyad: {soyad}, Notlar: {notlar}, Ortalama: {ortalama}\n")
+    dosya.close()
+
+
+while True:
+    ne_yapilacak = input("Ne yapacaksınız? (1: Kaydet, 0: Çık): ")
+
+    if ne_yapilacak == "0":
+        break
+
+    ad = input("Öğrenci Adı: ")
+    soyad = input("Öğrenci Soyadı: ")
+    notlar = input("Sınav notları (virgülle ayrılmış): ")
+    ogrenci_kaydet(ad, soyad, notlar)
